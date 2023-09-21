848e105c6f023d25c89f94974d20337d0b11f6d1
Philipp Rohde <Philipp.Rohde@tib.eu>
2023-09-21 09:35:19 +0200

    example: remove old usage of Trav-SHACL

--- example/validate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker exec -it travshacl_example_engine bash -c "python3 main.py -d /shapes/LUBM/ http://travshacl_example_data:8890/sparql /output/ DFS --heuristics TARGET IN BIG --orderby --selective --output"
